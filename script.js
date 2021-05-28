a27c610f0ff8bbc92ee4a8b1adb716c716ab5ab7
Newvolante <60291015+Newvolante@users.noreply.github.com>
2021-05-28 15:21:20 +0200

    added the logic to manage tags

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
 $(function() {
     console.log('Script linked');
+
+    // array to hold tags taken from images
+    let tags = [];
+    let taggedImages = {};
+
+    // iterating through images
+    $('img').each(function() {
+        console.warn('Image loop');
+        $img = $(this);
+        console.log($img);
+
+        let $tagString = $(this).data('tags');
+        
+        // array that contains the tags of the current image
+        let $arrayString = $tagString.split(',');
+        console.log('This image has the following tags: ' + $tagString);
+        
+        // adding new tags of the current image to the tags array
+        $arrayString.forEach(item => {
+            if (!tags.includes(item)) {
+                tags.push(item);
+            }
+            console.log('Unique tags so far: ' + tags);
+
+            //adding current tag as a property of the taggedImages array
+            if(taggedImages[item] == null) {
+                taggedImages[item] = [];
+            }
+            // adding image to the corresponding tags in the taggedImages array
+            taggedImages[item].push($img);
+        });
+
+        console.log('TaggedImages:', taggedImages['animals']);
+    });
+    console.log('Number of total tags: ' + tags.length);
 });
